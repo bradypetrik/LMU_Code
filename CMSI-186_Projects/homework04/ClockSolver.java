a5139016c604c45d81bf2b166380069f62438d19
bradypetrik <bradypetrik@gmail.com>
2017-03-16 12:24:52 -0700

    Update ClockSolver
    I was thinkin' too much

--- CMSI-186_Projects/homework04/ClockSolver.java
+++ CMSI-186_Projects/homework04/ClockSolver.java
@@ -57,7 +57,7 @@ public class ClockSolver {
                if(minutesAngle < hoursAngle) {
                   totalAngle = hoursAngle - minutesAngle;
                }
-               if(Math.abs(totalAngle - angle) <= 3) {
+               if(Math.abs(totalAngle - angle) <= 5) {
                   System.out.println(testClock.toString());
                }
             }
